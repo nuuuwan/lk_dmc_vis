056ca200943b7fe0435321ea335e0401506eafd6
nuuuwan <nuuuwan@gmail.com>
2025-11-29 10:04:03 +0530

    Fix formatting and alignment issues in ReadMe content for improved clarity

--- src/lk_dmc/readme/ReadMe.py
+++ src/lk_dmc/readme/ReadMe.py
@@ -8,28 +8,29 @@ class ReadMe:
 
     def get_lines(self) -> list[str]:
         return [
-            "# SriLanka 🇱🇰 - Flood Map",
+            "# #SriLanka 🇱🇰 - Flood Map",
             "",
             (
-                "This is a schematic diagram of river water levels "
-                "across gauging stations in Sri Lanka."
+                "This is a schematic diagram of river water levels"
+                + " across gauging stations in Sri Lanka."
             ),
             "",
             (
                 "Data from the "
-                "[Disaster Management Center](https://www.dmc.gov.lk/index.php) "
-                "of Sri Lanka."
+                + "[Disaster Management Center]"
+                + "(https://www.dmc.gov.lk/index.php)"
+                + " of Sri Lanka."
             ),
             "",
             (
-                "Updated whenever new information is released by the DMC, "
-                "usually every 3 hours."
+                "Updated whenever new information is released by the DMC,"
+                + " usually every 3 hours."
             ),
             "",
             (
-                "Image only link: "
-                "[https://nuuuwan.github.io/lk_dmc_vis]"
-                "(https://nuuuwan.github.io/lk_dmc_vis/)"
+                "Image only link:"
+                + " [https://nuuuwan.github.io/lk_dmc_vis]"
+                + "(https://nuuuwan.github.io/lk_dmc_vis/)"
             ),
             "",
             "![images/map.png](images/map.png)",
