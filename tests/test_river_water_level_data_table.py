7a787bbf9bd40d9a60638faa150e9404d7923f5c
nuuuwan <nuuuwan@gmail.com>
2026-08-11 13:14:22 +0530

    reran pipeline

--- tests/test_river_water_level_data_table.py
+++ tests/test_river_water_level_data_table.py
@@ -27,7 +27,6 @@ class TestCase(unittest.TestCase):
 
         rwld = t.d_list[0]
         actual_d = asdict(rwld)
-        print(actual_d)
         self.assertEqual(
             actual_d,
             {
